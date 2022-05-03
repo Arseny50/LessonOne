01ebb995a6dbf73e3b4c1a55a73509ccadc49d8d
Arseny <Zarubin93@icloud.com>
2022-05-03 15:32:50 +0300

    Add smth

--- src/main/java/ru/geekbrains/lesson1/TheThirdHometask.java
+++ src/main/java/ru/geekbrains/lesson1/TheThirdHometask.java
@@ -2,29 +2,33 @@ package ru.geekbrains.lesson1;
 
 public class TheThirdHometask {
     public static void main(String[] args) {
+        int[] arrSix = {1,3,5,7,9,11};
         theFirstObjective();
         theSecondObjective();
         theThirdObjective();
         theFourthObjective();
-        theFifthObjective();
+        theFifthObjective(7, 4);
+        theSixthObjective(arrSix);
+        //forExample();
     }
     public static void theFirstObjective() {
         int[] numbers = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 };
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 1 ) {
-                System.out.println(numbers[i] + 1);
+            if (numbers[i] == 0 ) {
+                numbers[i] = 1;
             }
             else {
-                System.out.println(numbers[i] -1 );
+                numbers[i] = 0;
             }
+            System.out.print(numbers[i] + " ");
         }
         }
 
     public static void theSecondObjective(){
         int[] anotherNumbers = new int [100];
-        anotherNumbers[0] = 0;
-        for (int i = 1; i < anotherNumbers.length; i++) {
-            System.out.println(i);
+        for (int i = 0; i < anotherNumbers.length; i++) {
+            anotherNumbers[i] = i +1;
+            System.out.println(anotherNumbers[i] + " ");
         }
 
     }
@@ -32,21 +36,67 @@ public class TheThirdHometask {
         int[] diffNumbers = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
         for (int i = 0; i < diffNumbers.length; i++) {
             if (diffNumbers[i] < 6) {
-                System.out.println( diffNumbers[i] * 2);
+                diffNumbers[i] = diffNumbers[i] * 2;
             }
+            System.out.println(diffNumbers[i] + " ");
         }
 
     }
     public static void theFourthObjective() {
-        int[][] cubeArr = new int[6][6];
+        int[][] cubeArr = { {6,6,6,6,6,6},
+                {6,6,6,6,6,6},
+                {6,6,6,6,6,6},
+                {6,6,6,6,6,6},
+                {6,6,6,6,6,6},
+                {6,6,6,6,6,6} };
         for (int i = 0; i < 6; i++) {
             for (int j = 0; j < 6; j++) {
                 if (i == j) {
-                    System.out.println(cubeArr[i][j] + 1);
+                    cubeArr[i][j] = 1;
                 }
+                System.out.print(cubeArr[i][j] + " ");
             }
+            System.out.println();
         }
     }
-    public static void theFifthObjective(){
+    public static void theFifthObjective(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print("[" + i + "]" + arr[i] + " ");
+        }
     }
+    public static void theSixthObjective( int[] arr) {
+    int max = arr[0];
+    int min = arr[0];
+            for (int i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+            for (int i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+            System.out.println(max);
+            System.out.println(min);
+}
+/*public static void forExample() {
+    int[] arrSix = {1,3,5,7,9,11};
+    int max = arrSix[0];
+    int min = arrSix[0];
+    for (int i = 0; i <arrSix.length; i++) {
+        if (arrSix[i] > max) {
+            max = arrSix[i];
+        }
+    }
+    for (int i = 0; i < arrSix.length; i++) {
+        if (arrSix[i] < min) {
+            min = arrSix[i];
+        }
+    }
+    System.out.println(max);
+    System.out.println(min);
+} */
 }
